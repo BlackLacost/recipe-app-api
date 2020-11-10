56ba44bc4ab8112e186750c5702fae16cf42e0aa
Ilya Lisin <blacklacost@gmail.com>
2020-11-10 18:26:17 +0300

    move tests to core

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = "test@test.com"
+        password = "Test123"
+        user = get_user_model().objects.create_user(email, password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@TEST.COM"
+        password = "Test123"
+        user = get_user_model().objects.create_user(email, password)
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        email = ""
+        password = "Test123"
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email, password)
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        email = "admin@test.com"
+        password = "Test123"
+        user = get_user_model().objects.create_superuser(email, password)
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
